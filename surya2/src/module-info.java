820ec85c2afa6310b03f430c36c1625bc796d669
SURYA <SURYA@Suryas-Laptop408.bbrouter>
2023-12-24 13:30:15 +0530

    First commit

---
+++ surya2/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author SURYA
+ *
+ */
+module surya2 {
+}
---
+++ surya2/src/surya2/compack1.java
@@ -0,0 +1,14 @@
+package surya2;
+
+public class compack1 {
+
+	public static void main(String[] args) {
+		
+		int a=10;
+		int b=20;
+		int c= a+b;
+		System.out.println(c);
+
+	}
+
+}
