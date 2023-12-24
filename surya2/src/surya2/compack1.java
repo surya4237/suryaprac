ad644641fb136c5aa9668daa7cdb582107652a6a
SURYA <SURYA@Suryas-Laptop408.bbrouter>
2023-12-24 13:36:02 +0530

    print start and print end

--- surya2/src/surya2/compack1.java
+++ surya2/src/surya2/compack1.java
@@ -4,10 +4,12 @@ public class compack1 {
 
 	public static void main(String[] args) {
 		
+		System.out.println("start");
 		int a=10;
 		int b=20;
 		int c= a+b;
 		System.out.println(c);
+		System.out.println("end");
 
 	}
 
